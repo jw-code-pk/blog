cd34f2eb09078436578753d8deacb1d56674fb06
Jevs <jewagenaar@gmail.com>
2025-06-14 19:58:49 +0100

    Clickable grid.

--- content/house_3/scripts/main.js
+++ content/house_3/scripts/main.js
@@ -1,11 +1,21 @@
-window.onload = main
+window.onload = main;
 
+function onGridClick(event) {
+  event.target.className = "container-element-01"
+}
 
-function main() {
+function populateGrid() {
   const buttonGrid = document.getElementById("button-grid");
 
   for (i = 0; i < 256; i++) {
     const currentDiv = document.createElement("div");
+    currentDiv.className = "container-element-hidden";
+    currentDiv.addEventListener("click", onGridClick, false);
     buttonGrid.appendChild(currentDiv);
   }
+}
+
+
+function main() {
+  populateGrid();
 }
