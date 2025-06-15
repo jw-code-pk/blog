ea49031c2314cd9e548238212095536c3d929354
Jevs <jewagenaar@gmail.com>
2025-06-15 15:06:30 +0100

    Add map and handle clicks based on coordinate and tile props.

--- content/house_3/scripts/main.js
+++ content/house_3/scripts/main.js
@@ -1,15 +1,67 @@
 window.onload = main;
 
-function onGridClick(event) {
-  event.target.className = "container-element-01"
+const map = [
+  "................",
+  "vvxxnx..........",
+  "vvxoox..........",
+  "vvxoox..........",
+  "xHxooxxxxnxxxx..",
+  "xoooooooooooox..",
+  "xxnxxxxoxxx.xx..",
+  "x....xooox...x..",
+  "x....xooox...x..",
+  "x....x...x...x..",
+  "x....x...x...x..",
+  "xxxxxx...xxxxxxx",
+  "..x............x",
+  "..x............x",
+  "..xxxxxxxxxxxxxx",
+  "................",
+]
+
+const props = {
+  "x": { "alert": "hi!", "type": "tile-x"  }
 }
+
+
+function onGridClick(event) {
+  const target = event.target;
+  const tileX = target.attributes["x"];
+  const tileY = target.attributes["y"]
+
+  const tile = map[tileY][tileX];
+  const p = props[tile];
+
+  if (p != null) {
+    const type = p["type"];
+
+    if (type != null) {
+      target.className = type; 
+    }
+  } else {
+    target.className = "tile-nothing";
+  }
+
+  console.log(`Handle click at: <${tileX}, <${tileY}>`);
+}
+
 
 function populateGrid() {
   const buttonGrid = document.getElementById("button-grid");
+  const mapDim = 16;
 
   for (i = 0; i < 256; i++) {
     const currentDiv = document.createElement("div");
-    currentDiv.className = "container-element-hidden";
+
+    const mapX = Math.floor(i % mapDim);
+    const mapY = Math.floor(i / mapDim);
+
+    const tile = map[mapY][mapX];
+
+    currentDiv.className = "tile-hidden";
+    currentDiv.attributes["x"] = mapX;
+    currentDiv.attributes["y"] = mapY;
+
     currentDiv.addEventListener("click", onGridClick, false);
     buttonGrid.appendChild(currentDiv);
   }
